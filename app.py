555c99c37699abfc70463fd07fc94cc21a725c3c
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-09-30 23:04:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def work():
 @app.route('/works')
 def works():
    try: 
-    return render_template('works.html', categories1=categories[1],categories2=categories[2],categories3=categories[3],categories4=categories[4],categories5=categories[5])
+    return render_template('works.html',categories0=categories[0], categories1=categories[1],categories2=categories[2],categories3=categories[3],categories4=categories[4],categories5=categories[5])
    except Exception as err:
        return str(err)
 @app.route('/about')
