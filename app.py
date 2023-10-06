b3b3e410b77a8e457c4dcb07c4ea53c6de5f2c8c
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-10-07 00:49:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import csv
 from flask import Flask, render_template, url_for, request, redirect
-app = Flask(__name__)
+from flask_cors import CORS  # Import the CORS module
 
+app = Flask(__name__)
+CORS(app)
 import time
 def get_data(json_file):
     try:
